8ae54a48fff422fe4244f13da0dd576305ef0b6f
GENTLEW1ND <rajchakraborty029@gmail.com>
2023-12-19 19:38:46 +0530

    Second commit

--- components/NavBar/Discover/Discover.jsx
+++ components/NavBar/Discover/Discover.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+// THIS COMPONENT IS FOR ROUNTING
+// AND NAVIGATION PU RPOSE.
 
+import React from "react";
+import Link from "next/link";
+
+// INTERNAL IMPORT
+import Style from "./Discover.module.css";
 const Discover = () => {
-  return (
-    <div>Discover</div>
-  )
-}
 
-export default Discover
+  // ----------DISCOVER NAVIGATION MENU
+  const discover =[
+    {
+      name:"Collection",
+      link: "collection",
+    },
+    {
+      name:"Search",
+      link: "search",
+    },
+    {
+      name:"Author Profile",
+      link: "author-profile",
+    },
+    {
+      name:"NFT Details",
+      link: "nft-details",
+    },
+    {
+      name:"Account Setting",
+      link: "account-setting",
+    },
+    {
+      name:"Connect Wallet",
+      link: "connect-wallet",
+    },
+    {
+      name:"Blog",
+      link: "blog",
+    }
+
+  ]
+  return ( 
+  <div>
+    {discover.map((el, i)=>(
+      <div key={i + 1} className={Style.discover}>
+        <Link href={{pathname:`${el.link}`}}>{el.name}</Link>
+      </div>
+    ))}
+  </div>
+  );
+};
+
+export default Discover;
--- components/NavBar/HelpCenter/HelpCenter.jsx
+++ components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+//  THIS IS USE FOR ROUTING AND 
+// NAVIGATION PURPOSE.
+import React from "react";
+import Link from "next/link";
 
+//  INTERNAL IMPORT
+import Style from "./HelpCenter.module.css";
 const HelpCenter = () => {
+  const helpCenter = [
+    {
+      name: "About",
+      link: "about",
+    },
+    {
+      name: "Contact us",
+      link: "contact-us",
+    },
+    {
+      name: "Sign up",
+      link: "sign-up",
+    },
+    {
+      name: "Sign in",
+      link: "sign-in",
+    },
+    {
+      name: "Subscription",
+      link: "subscritpion",
+    },
+  ];
   return (
-    <div>HelpCenter</div>
-  )
-}
+    <div className={Style.box}>
+      {
+        helpCenter.map((el, i)=>(
+          <div className={Style.helpCenter}>
+            <Link href={{pathname: `${el.link}`}}>{el.name}</Link>
+          </div>
+        ))
+      }
+    </div>
+    )
+};
 
-export default HelpCenter
+export default HelpCenter;
