3cd142d21abd584461ea685292bc8b412545a142
GENTLEW1ND <rajchakraborty029@gmail.com>
2024-01-05 12:52:30 +0530

    Fourteenth Commit

---
+++ collectionPage/collectionProfile/collectionProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//INTERNAL IMPORT 
+import Style from './collectionProfile.module.css'
+
+const collectionProfile = () => {
+  return (
+    <div>collectionProfile</div>
+  )
+}
+
+export default collectionProfile
