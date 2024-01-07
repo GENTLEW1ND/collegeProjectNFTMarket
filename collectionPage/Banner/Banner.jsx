a624ee468d3f7e20e3f416f205fc6361e881024b
GENTLEW1ND <rajchakraborty029@gmail.com>
2024-01-07 21:43:19 +0530

    Fifteenth Commit

--- collectionPage/Banner/Banner.jsx
+++ collectionPage/Banner/Banner.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-const Banner = () => {
+//INTERNAL IMPORT
+import Style from "./Banner.module.css";
+
+const Banner = ({ bannerImage }) => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className={Style.banner}>
+      <div className={Style.banner_img}>
+        <Image
+          src={bannerImage}
+          objectFit="cover"
+          alt="background"
+          width={1480}
+          height={350}
+        />
+      </div>
 
-export default Banner
+      <div className={Style.banner_img_mobile}>
+        <Image 
+          src={bannerImage}
+          objectFit="cover"
+          alt="background"
+          width={1600}
+          height={900}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- collectionPage/collectionProfile/collectionProfile.jsx
+++ collectionPage/collectionProfile/collectionProfile.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialInstagram
+} from "react-icons/ti";
 
-//INTERNAL IMPORT 
-import Style from './collectionProfile.module.css'
+//INTERNAL IMPORT
+import Style from "./collectionProfile.module.css";
+import images from "../../img";
 
 const collectionProfile = () => {
+  const cardAarray = [1, 2, 3, 4];
   return (
-    <div>collectionProfile</div>
-  )
-}
+    <div className={Style.collectionProfile}>
+      <div className={Style.collectionProfile_box}>
+        <div className={Style.collectionProfile_box_left}>
+          <Image
+            src={images.nft_image_1}
+            alt="nft image"
+            width={250}
+            height={250}
+            className={Style.collectionProfile_box_left_img}
+          />
 
-export default collectionProfile
+          <div className={Style.collectionProfile_box_left_social}>
+            <a href="#">
+              <TiSocialFacebook />
+            </a>
+            <a href="#">
+              <TiSocialInstagram />
+            </a>
+            <a href="#">
+              <TiSocialLinkedin />
+            </a>
+            <a href="#">
+              <TiSocialTwitter />
+            </a>
+          </div>
+        </div>
+
+        <div className={Style.collectionProfile_box_middle}>
+          <h1>Awesome NFTs collection</h1>
+          <p>
+            Karafuru is home to 5,555 generative arts where colors reign
+            supreme. Leave the drab reality and enter the world of Karafuru
+            Museum of Toys.
+          </p>
+
+          <div className={Style.collectionProfile_box_middle_box}>
+            {
+              cardAarray.map((el, i)=>(
+                <div className={Style.collectionProfile_box_middle_box_item} key={i + 1}>
+                  <small>Floor price</small>
+                  <p>${i + 1}95,3424</p>
+                  <span>+ {i + 2}.11%</span>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default collectionProfile;
