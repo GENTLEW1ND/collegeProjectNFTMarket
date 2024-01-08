a544f12d9ee58d612939f856dd985f4b1e1e20d0
GENTLEW1ND <rajchakraborty029@gmail.com>
2024-01-08 14:03:45 +0530

    Sixteenth Commit

---
+++ authorPage/AuthorProfileCard/AuthorProfileCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//INTERNAL IMPORT 
+import Style from './AuthorProfile.module.css'
+
+const authorProfileCard = () => {
+  return (
+    <div>authorProfileCard</div>
+  )
+}
+
+export default authorProfileCard
---
+++ authorPage/AuthorTabs/AuthorTabs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//INTERNAL IMPORT 
+import Style from './AuthorTabs.module.css'
+
+const AuthorTabs = () => {
+  return (
+    <div>AuthorTabs</div>
+  )
+}
+
+export default AuthorTabs
