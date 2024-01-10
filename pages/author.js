8ff948c24e901c61ed203f0f430f464afe3a5f04
GENTLEW1ND <rajchakraborty029@gmail.com>
2024-01-10 18:47:02 +0530

    Seventeenth Commit

--- pages/author.js
+++ pages/author.js
@@ -4,19 +4,40 @@ import React, { useState, useEffect } from 'react'
 import Style from '../styles/author.module.css'
 import { Banner } from '../collectionPage/collectionIndex'
 import { Brand, Title } from '../components/ComponentIndex'
+import FollowerTabCard from '../components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx'
 import images from '../img'
-import { AuthorProfileCard, AuthorTaps, TabCard } from '../authorPage/componentsIndex.jsx'
+import { AuthorProfileCard, AuthorTabs, AuthorNFTCardBox } from '../authorPage/componentsIndex.jsx'
 
 const author = () => {
-    const popularArray = [
-        images.user1,
-        images.user2,
-        images.user3,
-        images.user4,
-        images.user5,
-        images.user6,
-        images.user7,
-        images.user8,
+    const followingArray = [
+        {
+            background: images.creatorbackground1,
+            user: images.user1,
+        },
+        {
+            background: images.creatorbackground2,
+            user: images.user2,
+        },
+        {
+            background: images.creatorbackground3,
+            user: images.user3,
+        },
+        {
+            background: images.creatorbackground4,
+            user: images.user4,
+        },
+        {
+            background: images.creatorbackground5,
+            user: images.user5,
+        },
+        {
+            background: images.creatorbackground6,
+            user: images.user6,
+        },
+        {
+            background: images.creatorbackground7,
+            user: images.user7,
+        },
     ]
 
     const [collectiables, setCollectiables] = useState(true)
@@ -27,7 +48,30 @@ const author = () => {
     return (
         <div className={Style.banner}>
             <Banner bannerImage={images.creatorbackground2} />
-            <AuthorProfileCard/>
+            <AuthorProfileCard />
+            <AuthorTabs
+                setCollectiables={setCollectiables}
+                setCreated={setCreated}
+                setLike={setLike}
+                setFollower={setFollower}
+                setFollowing={setFollowing}
+            />
+            <AuthorNFTCardBox
+                collectiables={collectiables}
+                created={created}
+                like={like}
+                follower={follower}
+                following={following}
+            />
+            <Title heading="Popular creator" paragraph="Click on music icon and enjoy NFT music or audio " />
+            <div className={Style.author_box}>
+                {
+                    followingArray.map((el, i) => (
+                        <FollowerTabCard el={el} i={i} />
+                    ))
+                }
+            </div>
+            <Brand />
         </div>
     )
 }
